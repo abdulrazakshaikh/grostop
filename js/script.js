86cef841804edd910cb2b9208cd3aede0521606e
Abdul Razak Shaikh <89abdul@gmail.com>
2021-03-19 18:49:08 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,3 +20,19 @@ $('.dropdown').on('hide.bs.dropdown', function(e) {
 // $('.navbar .navbar-toggler').on('click', function(e) {
 //         $(this).toggleClass('active')
 // });    
+
+
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
