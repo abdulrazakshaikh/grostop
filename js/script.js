680db2529ca9dc859fe744cd224019f098f6125f
Abdul Razak Shaikh <89abdul@gmail.com>
2021-03-30 12:36:40 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ $(function () {
   })
 })
 
+// Filter
+$(function () {
+  'use strict'
+  $('[data-toggle="filterMenu"]').on('click', function () {
+    $('#filterMenu').toggleClass('open');
+    $('body').toggleClass('filtersidebaropen');
+  })
+})
+
+
+
 // Mega Dropdown Animated
 $('.dropdown').on('show.bs.dropdown', function(e) {
     $(this).find('.dropdown-menu').first().stop(!0, !0).slideDown(300)
@@ -20,6 +31,11 @@ $('.dropdown').on('hide.bs.dropdown', function(e) {
 // $('.navbar .navbar-toggler').on('click', function(e) {
 //         $(this).toggleClass('active')
 // });    
+
+$('.account-nav .nav-link').on('click', function(e) {
+    // $('.collapseAccountNav').removeClass('show');
+    $(".collapseAccountNav").collapse('hide')
+});    
 
 
 function increaseValue() {
@@ -36,3 +52,47 @@ function decreaseValue() {
   value--;
   document.getElementById('number').value = value;
 }
+
+
+// $(document).ready(function() {
+//    $('input[type="radio"]').click(function() {
+//        if($(this).attr('id') == 'otherstag') {
+//             $('#collapseOthers').show();           
+//        }
+//        else {
+//             $('#collapseOthers').hide();   
+//        }
+//    });
+// });
+
+
+// $(document).ready(function(){
+//     $('#otherstag').click(function(){
+       
+//     });
+// });
+$(document).ready(function() {
+    $("input[name$='tags']").click(function() {
+        var fieldval = $(this).val();
+
+        $("#collapseothers").hide();
+        $("#collapse" + fieldval).show();
+    });
+});
+
+// $('#hometag,#officetag,#otherstag').on('click', function (e) {
+//     e.stopPropagation();
+//     if (this.id == 'collapseOthers') {
+//         $('#collapseTwoPanel').collapse('hide');
+//         $('#collapseThreePanel').collapse('hide');
+//         $('#collapseOnePanel').collapse('show');
+//     } else if (this.id == 'collapseTwo') {
+//         $('#collapseOnePanel').collapse('hide');
+//         $('#collapseThreePanel').collapse('hide');
+//         $('#collapseTwoPanel').collapse('show');
+//     } else if (this.id == 'collapseThree') {
+//         $('#collapseOnePanel').collapse('hide');
+//         $('#collapseTwoPanel').collapse('hide');
+//         $('#collapseThreePanel').collapse('show');
+//     }
+// })
